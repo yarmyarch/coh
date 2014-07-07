2a7b3ef48f26d6ce96ed70c13635842600510529
Yujia Luo <yarmyarch@live.cn>
2014-07-07 21:46:37 +0800

    some sort of

---
+++ src/ui/coh.UIManager.js
@@ -0,0 +1,34 @@
+if ('mouse' in cc.sys.capabilities)
+    
+    //~ onMouseDown: null,
+    //~ onMouseUp: null,
+    //~ onMouseMove: null,
+    //~ onMouseScroll: null
+    
+    //
+    
+    cc.eventManager.addListener({
+        event: cc.EventListener.MOUSE,
+        onMouseMove: function(event){
+            if(event.getButton() != undefined)
+                event.getCurrentTarget().processEvent(event);
+        }
+    }, this);
+
+if (cc.sys.capabilities.hasOwnProperty('touches')){
+    cc.eventManager.addListener({
+        
+        //~ swallowTouches: false,
+        //~ onTouchBegan: null,
+        //~ onTouchMoved: null,
+        //~ onTouchEnded: null,
+        //~ onTouchCancelled: null,
+        
+        //
+        
+        event: cc.EventListener.TOUCH_ONE_BY_ONE,
+        onTouchesMoved:function (touches, event) {
+            event.getCurrentTarget().processEvent(touches[0]);
+        }
+    }, this);
+}
