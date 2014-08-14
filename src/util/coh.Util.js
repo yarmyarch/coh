5062180234c367725525ae5f8b58260a8e285647
Yujia Luo <yarmyarch@live.cn>
2014-08-14 18:48:54 +0800

    restore

---
+++ src/util/coh.Util.js
@@ -0,0 +1,105 @@
+var coh = coh || {};
+coh.Util = (function(){
+    var self;
+    
+    var LC = {
+        MAP_EN : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".split(''),
+        MAP_DE : {}
+    };
+    
+    var buf = {
+        riList : {0 : 1}
+    }
+    
+    // initialize map for base64.
+    for(var i=0; i<64; i++)
+        LC.MAP_DE[LC.MAP_EN[i]] = i;
+    
+    
+    return self = {
+        isExecutable : function(target) {
+            return target instanceof Function && (typeof(target)).toLowerCase() == "function";
+        },
+        
+        getRandom : function(arr) {
+            return arr[~~(Math.random() * arr.length)];
+        },
+        
+        popRandom : function(arr) {
+            var randomIndex = ~~(Math.random() * arr.length);
+                inst = arr[randomIndex];
+            
+            arr[randomIndex] = arr[arr.length - 1];
+            arr.length -= 1;
+            return inst;
+        },
+        
+        getRandId : function() {
+            var ri = 0,
+                _buf = buf;
+            while (_buf.riList[ri]) {
+                ri = (+(Math.random() + "").substring(2,10)).toString(36);
+            }
+            buf[ri] = 1;
+            return ri;
+        },
+        
+        /**
+         * return the a string whose first letter is in uppercase.
+         */
+        getFUStr : function(str) {
+            return str[0].toUpperCase() + str.substr(1);
+        },
+        
+        base64Encode : function(data) {
+            var buf = [],
+                map = LC.MAP_EN,
+                n = data.length,
+                val,
+                i = 0;
+
+            while(i < n) {
+                val = (data[ i ] << 16) |
+                    (data[i+1] << 8) |
+                    (data[i+2]);
+                
+                buf.push(map[val>>18],
+                    map[val>>12 & 63],
+                    map[val>>6 & 63],
+                    map[val & 63]);
+                i += 3;
+            }
+
+            if(n%3 == 1) buf.pop(),buf.pop(),buf.push('=', '=');
+            else buf.pop(),buf.push('=');
+
+            return buf.join('');
+        },
+
+        base64Decode : function(str) {
+            var buf = [],
+                arr = str.split(''),
+                map = LC.MAP_DE,
+                n = arr.length,
+                val,
+                i=0;
+
+            if(n % 4) return false;
+
+            while(i < n) {
+                val = (map[arr[ i ]] << 18) |
+                    (map[arr[i+1]] << 12) |
+                    (map[arr[i+2]] << 6)  |
+                    (map[arr[i+3]]);
+
+                buf.push(val>>16, val>>8 & 0xFF, val & 0xFF);
+                i += 4;
+            }
+
+            while(arr[--n] == '=')
+                buf.pop();
+
+            return buf;
+        }
+    };
+})();
