e0c8693ff456b929a7a25e87173fd2ff8a9d5760
Yujia Luo <yarmyarch@live.cn>
2014-09-04 12:43:29 +0800

    simple build

--- asset/coh.js
+++ asset/coh.js
@@ -265,8 +265,8 @@ coh.effects = {
     ranged : {
         // interested filters
         filters : {
-            getUnitPriorityInScene : function() {
-                
+            getUnitPriority : function(priority, unit) {
+                return priority - coh.LocalConfig.PRIORITY_CHUNK;
             }
         },
         // the effect won't gains with the level
@@ -779,75 +779,129 @@ coh.utils.FilterUtil = (function() {
     
     var self;
     
-    var buf = {
-        filters : {}
+    var g_utils = {
+        sort : function(a, b) {
+            return a - b;
+        }
     };
     
-    return self = {
+    self = {
         
         /**
-         * @param handler {Function} function(lastReturnedValue, [argument1, [argument2, [...]]])
-         * @param priority {int} defaultly set 10. The smaller, the higher priority.
+         * Activate an object as an adapter that can add and apply filters.
+         * Filters added to the given object would be reacted by itself only.
+         * @return {bool} false if there exist some a function having the same name with those to be added.
          */
-        addFilter : function(filterName, handler, priority) {
-            var _buf = buf,
-                priority = (undefined === priority) && 10 || priority;
+        activate : function(obj) {
             
-            !_buf.filters[filterName] && (_buf.filters[filterName] = {});
+            // the buffer used by this object only.
+            var buf = {
+                filters : {},
+                priorities : {}
+            };
             
-            if (!_buf.filters[filterName][priority]) {
-                _buf.filters[filterName][priority] = [];
-            }
-            
-            (handler instanceof Function) && _buf.filters[filterName][priority].push(handler);
-        },
-        
-        removeFilter : function(filterName, handler, priority) {
-            var _buf = buf,
-                filters, result = [],
-                priority = (undefined === priority) && 10 || priority;
-            
-            if (!_buf.filters[filterName]) return false;
-            
-            filters = _buf.filters[filterName][priority];
-            for (var i in filters) {
-                if (filters[i] != handler) result.push(filters[i]);
-            }
-            
-            _buf.filters[filterName][priority] = result;
-            return true;
-        },
-        
-        applyFilters : function(filterName, originalValue) {
-            var _buf = buf,
-                priorities = _buf.filters[filterName],
-                filterList,
-                filter,
-                value = originalValue,
-                arg = [],
-                i, j, leni, lenj;
-            
-            if (!priorities) return value;
-            
-            for (i in priorities) {
-                filterList = priorities[i];
-                for (j = 0, lenj = filterList.length; j < lenj; ++j) {
-                    // rebuild arguments for next filter. 
-                    // first param is currend calculated value, additional arguments can be parsed via the input.
-                    arg = value == undefined ? (arg.length && arg || [].slice.call(arguments, 1)) : [value].concat([].slice.call(arguments, 2));
-                    (filterList[j] instanceof Function) && (value = filterList[j].apply({}, arg));
+            var expandations = {
+                
+                /**
+                 * @param handler {Function} function(lastReturnedValue, [argument1, [argument2, [...]]])
+                 * @param priority {int} defaultly set 10. The smaller, the higher priority. Should be bigger than 0.
+                 */
+                addFilter : function(filterName, handler, priority) {
+                    var _buf = buf,
+                        priority = (undefined === priority) && 10 || priority;
+                    
+                    if (!(handler instanceof Function)) {
+                        return false;
+                    }
+                    
+                    !_buf.filters[filterName] && (_buf.filters[filterName] = {}, _buf.priorities[filterName] = []);
+                    
+                    if (!_buf.filters[filterName][priority]) {
+                        _buf.priorities[filterName].push(priority);
+                        _buf.priorities[filterName].sort(g_utils.sort);
+                        _buf.filters[filterName][priority] = [];
+                    }
+                    
+                    _buf.filters[filterName][priority].push(handler);
+                    
+                    return true;
+                },
+                
+                removeFilter : function(filterName, handler, priority) {
+                    var _buf = buf,
+                        filters, result = [],
+                        priority = (undefined === priority) && 10 || priority;
+                    
+                    if (!_buf.filters[filterName]) return false;
+                    
+                    filters = _buf.filters[filterName][priority];
+                    for (var i = 0, filter; filter = filters[i]; ++i) {
+                        if (filter != handler) result.push(filter);
+                    }
+                    // all filters removed, remove the priority from buffer.
+                    if (!_buf.filters[filterName][priority].length) {
+                        delete _buf.filters[filterName][priority];
+                        for (i = 0, len = _buf.priorities[filterName].length; i < len; ++i) {
+                            if (_buf.priorities[filterName][i] == priority) {
+                                _buf.priorities[filterName][i] = Number.POSITIVE_INFINITY;
+                                _buf.priorities[filterName].sort(g_utils.sort);
+                            }
+                        }
+                    }
+                    
+                    _buf.filters[filterName][priority] = result;
+                    return true;
+                },
+                
+                applyFilters : function(filterName, originalValue) {
+                    var _buf = buf,
+                        priorities = _buf.priorities[filterName],
+                        filterList,
+                        filter,
+                        value = originalValue,
+                        arg = [],
+                        i, j, leni, lenj;
+                    
+                    if (!priorities || !priorities.length) return value;
+                    
+                    for (i = 0, len = priorities.length; i < len; ++i) {
+                        filterList = _buf.filters[filterName][priorities[i]];
+                        for (j = 0, lenj = filterList.length; j < lenj; ++j) {
+                            // rebuild arguments for next filter. 
+                            // first param is currend calculated value, additional arguments can be parsed via the input.
+                            arg = value == undefined ? (arg.length && arg || [].slice.call(arguments, 1)) : [value].concat([].slice.call(arguments, 2));
+                            (filterList[j] instanceof Function) && (value = filterList[j].apply({}, arg));
+                        }
+                    }
+                    
+                    return value;
+                },
+                
+                clearFilters : function(filterName) {
+                    var _buf = buf;
+                    
+                    _buf.filters[filterName] = {};
+                    
+                    return true;
                 }
+            };
+            
+            var i;
+            // in case of occupations.
+            for (i in expandations) {
+                if (obj[i]) return false;
             }
-            
-            return value;
-        },
-        
-        clearFilters : function(filterName) {
-            var _buf = buf;
-            
-            _buf.filters[filterName] = {};
+            for (i in expandations) {
+                obj[i] = expandations[i];
+            }
+            return true;
         }
     }
+    
+    // register the object itself as an adapter.
+    self.activate(self);
+    
+    return self;
     
 })();/**
  * Calculator used to get basic attributes of a unit.
@@ -2779,7 +2833,7 @@ coh.BattleScene = function() {
             // XXXXXX TODO: ranged ones.
             priority = (unitBody.unit.getMode() + _lc.PRIORITIES.PHALANX) * _lc.PRIORITY_CHUNK + priority;
             
-            return coh.FilterUtil.applyFilters("getUnitPriorityInScene", priority, unitBody.unit);
+            return priority;
         },
         
         /**
